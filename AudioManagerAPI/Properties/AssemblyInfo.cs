1ba673cc5207c9232243f31dc36eeac222f26237
iomatix <mateuszwypchlak@gmail.com>
2026-06-01 19:09:27 +0200

    2.0.3

--- AudioManagerAPI/Properties/AssemblyInfo.cs
+++ AudioManagerAPI/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.2")]
-[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3")]
+[assembly: AssemblyFileVersion("2.0.3")]
 [assembly: NeutralResourcesLanguage("en")]
