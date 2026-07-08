9a2255f86f86295ba0c8df348df10251f6982728
iomatix <mateuszwypchlak@gmail.com>
2026-07-08 12:16:40 +0200

    Warn logging moved from DEBUG to prod too

--- AudioManagerAPI/Logger/ApiLogger.cs
+++ AudioManagerAPI/Logger/ApiLogger.cs
@@ -6,7 +6,6 @@
         [System.Diagnostics.Conditional("DEBUG")]
         public static void Debug(string message) => Log.Debug(message);
 
-        [System.Diagnostics.Conditional("DEBUG")]
         public static void Warn(string message) => Log.Warn(message);
 
         public static void Info(string message) => Log.Info(message);
